0ad716819e085220670f6018a164ff35827555b7
Ruurd Bijlsma <ruurd@bijlsma.dev>
2025-01-24 22:19:14 +0100

    Remove slow tests

--- tests/machine_learning/test_captioner.py
+++ tests/machine_learning/test_captioner.py
@@ -35,26 +35,3 @@ def test_blip_captioner_conditional(
     assert len(caption) > min_caption_length
 
 
-@pytest.mark.parametrize(
-    "llm_provider",
-    [
-        pytest.param(LLMProvider.MINICPM, marks=pytest.mark.cuda),
-        LLMProvider.OPENAI,
-    ],
-)
-def test_minicpm_captioner(
-    assets_folder: Path,
-    llm_provider: LLMProvider,
-) -> None:
-    """Test the LLMCaptioner with MiniCPM and OpenAI."""
-    if llm_provider == LLMProvider.OPENAI and os.environ.get("OPENAI_API_KEY") is None:
-        # Only run test if OPENAI_API_KEY is set.
-        pytest.skip("OPENAI_API_KEY is not set, so OpenAI captioning test is skipped.")
-
-    image = Image.open(assets_folder / "sunset.jpg")
-    llm = LLMCaptioner(LLMProvider.MINICPM)
-    caption = llm.caption(image)
-    print(caption)
-    assert isinstance(caption, str)
-    min_caption_length = 3
-    assert len(caption) > min_caption_length
